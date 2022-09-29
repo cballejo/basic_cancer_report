9a8c0e6498e1a83f53ece335d30d06340ffc9002
cballejo <christianballejo@gmail.com>
2022-09-29 09:20:26 -0300

    incoporamos carpetas por etapa

---
+++ data_processing/Database leaker.R
@@ -0,0 +1,26 @@
+## Packages
+
+library(tidyverse)
+library(lubridate)
+
+
+## Read database
+
+Consultas_codificadas <- read_csv("data_processing/data_scrap.csv") %>% 
+  mutate(`FECHA RECEP` = dmy(fecha))  %>%  select(-fecha) 
+
+names(Consultas_codificadas) <- str_to_upper(names(Consultas_codificadas))
+
+POSITIVOS <- Consultas_codificadas %>% 
+  detect_cancer() %>%
+  mutate (AÑO = year(`FECHA RECEP`)) %>%  
+  filter(CANCER == "SI") %>% 
+  select(SEXO, `FECHA RECEP` , DIAGNOSTICO, AÑO) %>% 
+  arrange(`FECHA RECEP`) 
+
+write_excel_csv(POSITIVOS, "data_processed.csv")  
+
+
+
+
+
